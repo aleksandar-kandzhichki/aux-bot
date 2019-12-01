7cb7953861d1da5e12bb0bc5de88e762c1e4356b
Aleksandar Kandzhichki <ak@auxcode.com>
2019-12-01 22:50:35 +0200

    multi-line search. Grouping of messages

--- backend/bot.ts
+++ backend/bot.ts
@@ -9,6 +9,8 @@ const client = new Discord.Client();
 const discordCommands = new DiscordCommandReader(client);
 const chatHistoryService = new DiscordChatHistory(client);
 
+type processedOrder = { name: string, order: string };
+
 discordCommands.registerCommandNames(Object.values(CommandNames));
 discordCommands.commands.subscribe(async c => {
   console.log(c);
@@ -24,10 +26,18 @@ discordCommands.commands.subscribe(async c => {
     history = history.filter(msg => msg.content.toLowerCase().includes(c.params.from as string))
   }
   if (c.params.date) fromDate = new Date(Date.parse(c.params.date as string));
-  history = history.filter(msg => isSameDay(msg.createdAt, fromDate))
-  c.channel.sendMessage(`checking ${history.length} messages!`);
-  return c.channel.sendMessage(history.map(msg => `${msg.member.displayName}: ${msg.content}`).join('  \n'));
 
+  history = history.filter(msg => isSameDay(msg.createdAt, fromDate))
+  c.channel.sendMessage(`checking ${history.length} messages... \n\n #Summary: \n\n`);
+  let lines: processedOrder[] = history.map(msg => ({ name: msg.member.displayName, order: extractOrder(msg.content, c.params.from as string)! }));
+  c.channel.sendMessage(lines.map(msg => `${msg.name}: ${msg.order}`).join('  \n'));
+  c.channel.sendMessage("\n\n # Grouping... \n\n");
+
+  let grouped = groupByOrder(lines);
+
+  c.channel.sendMessage(Object.keys(grouped).map(k => `${grouped[k]}x ${k}`).join('  \n'));
+
+  return;
   // fetching messages by date with -date dateStr
   // let date: Date | undefined;
   // if (c.params.date) {
@@ -45,8 +55,21 @@ discordCommands.commands.subscribe(async c => {
 //   let history = await chatHistoryService.getByDate(date, channel);
 //   channel.sendMessage(`checking ${history.length} messages!`);
 // }
+
+function extractOrder(text: string, searchStr?: string) {
+  if (!searchStr) return text;
+  return text.split("\n").find(line => line.toLowerCase().includes(searchStr))?.toLocaleLowerCase().replace(searchStr.toLowerCase(), "").replace(":", "").replace("-", "").trim();
+}
 function isSameDay(d1: Date, d2: Date) {
   return d1.getDate() == d2.getDate() && d1.getMonth() == d2.getMonth() && d1.getFullYear() == d2.getFullYear()
+}
+function groupByOrder(history: processedOrder[]): { [order: string]: number } {
+  let wishes = history.reduce((obj, cur) => {
+    if (obj[cur.order]) obj[cur.order]++;
+    else obj[cur.order] = 1;
+    return obj
+  }, {} as any)
+  return wishes;
 }
 
 client.login(auth.token);
