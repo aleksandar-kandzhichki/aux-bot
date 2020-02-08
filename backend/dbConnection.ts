60ec26009aff288eca649f9caa7403876bccb0c5
Aleksandar Kandzhichki <akandzhichki@gmail.com>
2020-02-08 12:14:08 +0200

    Add external DB

--- backend/dbConnection.ts
+++ backend/dbConnection.ts
@@ -3,9 +3,10 @@ import { connect } from 'mongoose';
 const options = {
     useNewUrlParser: true,
 };
+const dbPort = 3327;
 
 export function mongoConnect() {
-    return connect('mongodb://localhost:27017/AuxBot', options)
+    return connect(`mongodb://dev.auxcode.com:${dbPort}/AuxBot`, options)
         .catch(error => {
             console.log(`Error occured while connecting to mongo - ${error}`);
             return error;
