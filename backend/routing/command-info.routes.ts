3b9875d5a7b8c9ae0a8ecc2f825294025589cf3e
Hristiyana Panayotova <hrisi.p.panayotova@gmail.com>
2020-02-11 10:19:34 +0200

    adding authentication middleware

--- backend/routing/command-info.routes.ts
+++ backend/routing/command-info.routes.ts
@@ -1,23 +1,23 @@
 import * as express from 'express';
 import defaultCommandInfoStorage from '../storage/command-info.storage';
-import { wrapper } from './wrapper';
+import { authenticate } from './auth-middleware';
 
 const router = express.Router();
 
 
-router.post('/api/command-info/add', wrapper(async (req, res) => {
+router.post('/api/command-info/add', authenticate, async (req, res) => {
     await defaultCommandInfoStorage.addNewCommands(req.body.commands);
     res.sendStatus(201);
-}));
+});
 
-router.get('/api/command-info/commands', wrapper(async (_req, res) => {
+router.get('/api/command-info/commands', authenticate, async (_req, res) => {
     const commandInfo = await defaultCommandInfoStorage.getAllCommands();
     res.json(commandInfo);
-}));
+});
 
-router.put('/api/command-info/command', wrapper(async (_req, res) => {
+router.put('/api/command-info/command', authenticate, async (_req, res) => {
     await defaultCommandInfoStorage.updateCommandInfo(_req.body.command);
     res.sendStatus(204);
-}));
+});
 
 export default router;
--- backend/routing/scheduled-messages.routes.ts
+++ backend/routing/scheduled-messages.routes.ts
@@ -1,23 +1,23 @@
 import * as express from 'express';
 import scheduledMessagestorage from '../storage/scheduled-message.storage';
-import { wrapper } from './wrapper';
+import { authenticate } from './auth-middleware';
 
 const router = express.Router();
 
 
-router.post('/api/scheduled-messages/add', wrapper(async (req, res) => {
+router.post('/api/scheduled-messages/add', authenticate, async (req, res) => {
     await scheduledMessagestorage.addNewScheduledMessage(req.body.message);
     res.sendStatus(201);
-}));
+});
 
-router.get('/api/scheduled-messages/messages', wrapper(async (_req, res) => {
+router.get('/api/scheduled-messages/messages', authenticate, async (_req, res) => {
     const messages = await scheduledMessagestorage.getAllMessages();
     res.json(messages);
-}));
+});
 
-router.put('/api/scheduled-messages/message', wrapper(async (_req, res) => {
+router.put('/api/scheduled-messages/message', authenticate, async (_req, res) => {
     await scheduledMessagestorage.updateMessageById(_req.body.message);
     res.sendStatus(204);
-}));
+});
 
 export default router;
